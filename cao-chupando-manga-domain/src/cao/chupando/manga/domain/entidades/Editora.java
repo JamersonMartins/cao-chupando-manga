6f7747763f093954249b6afe5ae868cc8c508d18
jamesparta <jamersonmartins1@gmail.com>
2019-07-31 16:41:24 -0400

    Adicionando interface

--- cao-chupando-manga-domain/src/cao/chupando/manga/domain/entidades/Editora.java
+++ cao-chupando-manga-domain/src/cao/chupando/manga/domain/entidades/Editora.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cao.chupando.manga.domain.entidades;
 
-/**
- *
- * @author aluno
- */
+
 public class Editora {
+    
+    private int it;
+    private String nome;
+    private String nacionalidade;
+
+    public int getIt() {
+        return it;
+    }
+
+    public void setIt(int it) {
+        this.it = it;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+    
+    
     
 }
