329fd5308e414cb3d9d62b3c25765be79c1c9037
Your Name <you@example.com>
2019-08-07 17:00:39 -0400

    Atualizando

--- cao-chupando-manga-webapi/src/java/controller/CategoriaController.java
+++ cao-chupando-manga-webapi/src/java/controller/CategoriaController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/categoria")
 public class CategoriaController {
     
+    @GET
     @Path("/")
     public String index(){
         return "teste";
